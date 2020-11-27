b68c9f52e0690aa7c0eaf3eabcf8bcdf59086715
Funnisimo <scastette@yahoo.com>
2020-11-27 11:50:46 -0600

    redid wordWrap

--- ts/lines.spec.ts
+++ ts/lines.spec.ts
@@ -7,37 +7,40 @@ describe('wordWrap', () => {
   test('nextBreak', () => {
     expect(Lines.nextBreak('test', 0)).toEqual([4, 4]);
 
-    expect(Lines.nextBreak('test test', 0)).toEqual([5, 4]);
+    expect(Lines.nextBreak('test test', 0)).toEqual([4, 4]);
     expect(Lines.nextBreak('test test', 5)).toEqual([9, 4]);
 
-    expect(Lines.nextBreak('test test test', 5)).toEqual([10, 4]);
+    expect(Lines.nextBreak('test test test', 5)).toEqual([9, 4]);
     expect(Lines.nextBreak('test test test', 10)).toEqual([14, 4]);
 
-    expect(Lines.nextBreak('test tests test', 0)).toEqual([5,4]);
-    expect(Lines.nextBreak('test tests test', 5)).toEqual([11,5]);
+    expect(Lines.nextBreak('test tests test', 0)).toEqual([4,4]);
+    expect(Lines.nextBreak('test tests test', 5)).toEqual([10,5]);
     expect(Lines.nextBreak('test tests test', 11)).toEqual([15,4]);
 
     expect(Lines.nextBreak('ΩorangeΩtest∆', 0)).toEqual([13,4]);
 
-    expect(Lines.nextBreak('ΩorangeΩtest test test∆', 0)).toEqual([13,4]);
-    expect(Lines.nextBreak('ΩorangeΩtest test test∆', 13)).toEqual([18,4]);
+    expect(Lines.nextBreak('ΩorangeΩtest test test∆', 0)).toEqual([12,4]);
+    expect(Lines.nextBreak('ΩorangeΩtest test test∆', 13)).toEqual([17,4]);
     expect(Lines.nextBreak('ΩorangeΩtest test test∆', 18)).toEqual([23,4]);
 
-    expect(Lines.nextBreak('test ΩorangeΩtest test∆', 0)).toEqual([5,4]);
-    expect(Lines.nextBreak('test ΩorangeΩtest test∆', 5)).toEqual([18,4]);
+    expect(Lines.nextBreak('test ΩorangeΩtest test∆', 0)).toEqual([4,4]);
+    expect(Lines.nextBreak('test ΩorangeΩtest test∆', 5)).toEqual([17,4]);
     expect(Lines.nextBreak('test ΩorangeΩtest test∆', 18)).toEqual([23,4]);
 
-    expect(Lines.nextBreak('test ΩorangeΩtests∆ test', 0)).toEqual([5,4]);
-    expect(Lines.nextBreak('test ΩorangeΩtests∆ test', 5)).toEqual([20,5]);
+    expect(Lines.nextBreak('test ΩorangeΩtests∆ test', 0)).toEqual([4,4]);
+    expect(Lines.nextBreak('test ΩorangeΩtests∆ test', 5)).toEqual([19,5]);
     expect(Lines.nextBreak('test ΩorangeΩtests∆ test', 20)).toEqual([24,4]);
 
-    expect(Lines.nextBreak('tests tests tests', 0)).toEqual([6,5]);
-    expect(Lines.nextBreak('tests tests tests', 6)).toEqual([12,5]);
+    expect(Lines.nextBreak('tests tests tests', 0)).toEqual([5,5]);
+    expect(Lines.nextBreak('tests tests tests', 6)).toEqual([11,5]);
     expect(Lines.nextBreak('tests tests tests', 12)).toEqual([17,5]);
 
-    expect(Lines.nextBreak('tests ΩorangeΩtests∆ tests', 0)).toEqual([6,5]);
-    expect(Lines.nextBreak('tests ΩorangeΩtests∆ tests', 6)).toEqual([21,5]);
+    expect(Lines.nextBreak('tests ΩorangeΩtests∆ tests', 0)).toEqual([5,5]);
+    expect(Lines.nextBreak('tests ΩorangeΩtests∆ tests', 6)).toEqual([20,5]);
     expect(Lines.nextBreak('tests ΩorangeΩtests∆ tests', 21)).toEqual([26,5]);
+
+    expect(Lines.nextBreak('test ΩorangeΩtests∆test', 0)).toEqual([4,4]);
+    expect(Lines.nextBreak('test ΩorangeΩtests∆test', 5)).toEqual([23,9]);
     
   });
 
@@ -61,6 +64,10 @@ describe('wordWrap', () => {
 
     expect(Lines.wordWrap('tests tests tests', 10)).toEqual('tests\ntests\ntests');
     expect(Lines.wordWrap('tests ΩorangeΩtests∆ tests', 10)).toEqual('tests\nΩorangeΩtests∆\ntests');
+  });
+  
+  test('colors ending mid-sequence', () => {
+    expect(Lines.wordWrap('test ΩorangeΩtests∆test', 10)).toEqual('test\nΩorangeΩtests∆test');
   });
   
 });
--- ts/lines.ts
+++ ts/lines.ts
@@ -13,7 +13,7 @@ export function nextBreak(text:string, start:number) {
   while(i < text.length) {
     const ch = text[i];
     if (ch == ' ') {
-      while(text[i] == ' ') ++i;
+      while(text[i + 1] == ' ') ++i;
       return [i, l];
     } 
     if (ch == '\n') {
@@ -45,39 +45,95 @@ export function nextBreak(text:string, start:number) {
 }
 
 
-export function wordWrap(text:string, width:number, indent:number=0) {
+// export function wordWrap(text:string, width:number, indent:number=0) {
+// 
+//   if (text.length <= width) return text;
+// 
+//   let left = width;
+//   const maxWidth = width - indent;
+//   let start = 0;
+//   let output = '';
+// 
+//   let i = 0;
+//   while( i < text.length) {
+//     const [j,l] = nextBreak(text, i);
+//     // j = index of space
+//     left -= (l + ((j<text.length) ? 1 : 0));
+//     if (left > 0) {
+//       i = j;
+//     }
+//     else if (left == 0 || left == -1) {
+//       output += text.substring(start, j-1);
+//       if (j < text.length) output += '\n';
+//       start = i = j;
+//       left = maxWidth;
+//     }
+//     else {  // over (i == space)
+//       output += text.substring(start, i - 1) + '\n';
+//       start = i;
+//       left = maxWidth;
+//     }
+//   }
+// 
+//   if (start == 0) return text;
+// 
+//   if (start < text.length) {
+//     output += text.substring(start);
+//   }
+//   return output;
+// }
 
-  if (text.length <= width) return text;
 
-  let left = width;
-  const maxWidth = width - indent;
-  let start = 0;
-  let output = '';
+export function splice(text:string, start:number, len:number, add:string='') {
   
-  let i = 0;
-  while( i < text.length) {
-    const [j,l] = nextBreak(text, i);
-    // j = index of space
-    left -= (l + 1);
-    if (left > 0) {
-      i = j;
+  return text.substring(0, start) + add + text.substring(start+len);
+}
+
+
+// Returns the number of lines, including the newlines already in the text.
+// Puts the output in "to" only if we receive a "to" -- can make it null and just get a line count.
+export function wordWrap(text:string, width:number, indent=0) {
+  let w, textLength;
+  let spaceLeftOnLine, wordWidth;
+
+  if (!width) throw new Error('Need string and width');
+  const firstWidth = width;
+  width = width - indent;
+
+  let printString = text;
+  textLength = printString.length; // do NOT remove color length
+
+  // Now go through and replace spaces with newlines as needed.
+
+  // Fast foward until i points to the first character that is not a color escape.
+  // for (i=0; printString.charCodeAt(i) == COLOR_ESCAPE; i+= 4);
+  spaceLeftOnLine = firstWidth;
+
+  // console.log('wordWrap - ', text, width, indent);
+
+  let i = -1;
+  while (i < textLength) {
+    // wordWidth counts the word width of the next word without color escapes.
+    // w indicates the position of the space or newline or null terminator that terminates the word.
+    ([w,wordWidth] = nextBreak(printString, i + 1));
+
+    // console.log('- w=%d, width=%d, space=%d', w, wordWidth, spaceLeftOnLine)
+
+    if (wordWidth == spaceLeftOnLine) {
+      const nl = (w < textLength) ? '\n' : '';
+      printString = splice(printString, w, 1, nl);	// [i] = '\n';
+      spaceLeftOnLine = width; 
     }
-    else if (left == 0 || left == -1) {
-      output += text.substring(start, j-1) + '\n';
-      start = i = j;
-      left = maxWidth;
+    else if (wordWidth > spaceLeftOnLine || printString[i] === '\n') {
+      printString = splice(printString, i, 1, '\n');	// [i] = '\n';
+      spaceLeftOnLine = width - wordWidth - 1; // line width minus the width of the word we just wrapped and the space
+      //printf("\n\n%s", printString);
+    } else {
+      spaceLeftOnLine -= 1 + wordWidth;
     }
-    else {  // over (i == space)
-      output += text.substring(start, i - 1) + '\n';
-      start = i;
-      left = maxWidth;
-    }
+
+    i = w; // Advance to the terminator that follows the word.
   }
 
-  if (start == 0) return text;
-  
-  if (start < text.length) {
-    output += text.substring(start);
-  }
-  return output;
+  return printString;
 }
